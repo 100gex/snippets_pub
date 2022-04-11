0f0bd19fc1b490f01477720e40d5a4d7d412347e
100gex <40779170+100gex@users.noreply.github.com>
2022-04-11 15:49:32 +1000

    Formatting.

--- react_list_mutation.js
+++ react_list_mutation.js
@@ -3,33 +3,33 @@
 import { useState } from "react";
 
 function List(props) {
-  return (
-    <div className="List">
-      {props.items.map((item, idx) => (
-        <li>
-          {idx} - {item.message}
-        </li>
-      ))}
-    </div>
-  );
+    return (
+        <div className="List">
+            {props.items.map((item, idx) => (
+                <li>
+                    {idx} - {item.message}
+                </li>
+            ))}
+        </div>
+    );
 }
 
 export default function App() {
-  let [mutCount, setMutCount] = useState(0);
-  let [items, setItems] = useState([
-    { message: "Foo" },
-    { message: "Bar" },
-  ]);
-  let mutItems = () => {
-    items[0].message += "s"; // The idiomatic way to do this is to create a new list and call setItems with the new list
-    setMutCount(mutCount + 1); // Note if this isn't done, the update to items never occurs. This is even if we mutate the list and then call setItems(items)
-    console.log("mut");
-  };
-  return (
-    <div className="App">
-      <p>mut count: {mutCount}</p>
-      <List items={items} />
-      <button onClick={mutItems}>mutate</button>
-    </div>
-  );
+    let [mutCount, setMutCount] = useState(0);
+    let [items, setItems] = useState([
+        { message: "Foo" },
+        { message: "Bar" },
+    ]);
+    let mutItems = () => {
+        items[0].message += "s"; // The idiomatic way to do this is to create a new list and call setItems with the new list
+        setMutCount(mutCount + 1); // Note if this isn't done, the update to items never occurs. This is even if we mutate the list and then call setItems(items)
+        console.log("mut");
+    };
+    return (
+        <div className="App">
+            <p>mut count: {mutCount}</p>
+            <List items={items} />
+            <button onClick={mutItems}>mutate</button>
+        </div>
+    );
 }
--- vue_list_mutation.vue
+++ vue_list_mutation.vue
@@ -1,22 +1,20 @@
 <!-- Code tested in App.vue on Vuew playground: https://sfc.vuejs.org/ -->
 <script>
 export default {
-  data() {
-  	return {
-    	items: [{ message: 'Foo' }, { message: 'Bar' }]
-  	}
+	data() {
+		return {
+			items: [{ message: 'Foo' }, { message: 'Bar' }]
+		}
 	},
-  methods: {
-  	mutate() {
-      this.items[0].message += 's'
-    }
+	methods: {
+		mutate() {
+			this.items[0].message += 's'
+		}
 	}
 }
 </script>
 
 <template>
-	<li v-for="(item, index) in items">
-  	{{index }} - {{ item.message }}
-	</li>
-  <button @click="mutate">mutate</button>
+	<li v-for="(item, index) in items">{{ index }} - {{ item.message }}</li>
+	<button @click="mutate">mutate</button>
 </template>
